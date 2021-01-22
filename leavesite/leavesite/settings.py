7f7ccb12dfc833de8fb36ecf1247f93146c297af
Shareena de la Cruz <sdelacruz@equuscs.com>
2021-01-22 08:36:19 +0800

    remove secret key not in use

--- leavesite/leavesite/settings.py
+++ leavesite/leavesite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-$e6&2z1ndc*+uos)8s2kfo6haj$^s4m=gn7)8lmylr3ekx^a4'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
